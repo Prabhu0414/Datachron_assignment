1a45c62150af5a53e64f56cebb86ad24a031c934
Prabhu0414 <tanmayyadav1410@gmail.com>
2025-09-07 18:02:26 +0530

    adding backend url

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL || "https://datachron-assignment.onrender.com/";
 
 // generic request
 async function request(path, method = "GET", body, token) {
